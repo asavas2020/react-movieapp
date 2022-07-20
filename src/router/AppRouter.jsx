99a115660f979f7a0ae8b56db7650fd7e69d3aa4
Gokhan <gknsvsdata@gmail.com>
2022-07-20 14:11:26 +0300

    register

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,26 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from "../pages/Main"
+import Login from "../pages/Login"
+import MovieDetail from "../pages/MovieDetail"
+import Register from "../pages/Register"
+import Navbar from '../components/Navbar';
+
+const AppRouter = () => {
+  return (
+
+    <BrowserRouter> 
+    <Navbar/>
+      <Routes>
+        <Route path="/" element ={<Main/>}/>
+        <Route path="/login" element ={<Login/>}/>
+        <Route path="/register" element ={<Register/>}/>
+        <Route path="/details/:id" element ={<MovieDetail/>}/>
+      </Routes>
+     
+    </BrowserRouter>
+   
+  );
+};
+
+export default AppRouter;
